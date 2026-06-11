efdfa7faa75108842a3c2c54c02271767fc51635
Rohith kumar R <rohithkumarrakasy@gmail.com>
2026-06-10 17:54:53 -0700

    Update: Added more functionality in excel to filter and update values in each cell

--- ExcelJs_Util/excelPrac.js
+++ ExcelJs_Util/excelPrac.js
@@ -1,17 +1,69 @@
-const excelJs = require('exceljs');
+const excelJs = require("exceljs");
 
-const workbook = new excelJs.Workbook();
+// async function excelTest() {
+//   const workbook = new excelJs.Workbook();
+//   await workbook.xlsx.readFile(
+//     "C:/Users/rohit/Downloads/excel_download_test.xlsx",
+//   );
+//   const worksheet = await workbook.getWorksheet("Sheet1");
 
-async function excelTest(){
+//   worksheet.eachRow((row, rowNumber) => {
+//     row.eachCell((cell, colNumber) => {
+//       console.log(cell.value);
+//     });
+//   });
+// }
+
+// excelTest();
+
+// /* Find particular value in excel */
+
+// async function writeUpdateExcel() {
+    
+//     const workbook = new excelJs.Workbook();
+//     await workbook.xlsx.readFile("C:/Users/rohit/Downloads/excel_download_test.xlsx");
+//     const worksheet = workbook.getWorksheet("Sheet1");
+//     worksheet.eachRow((row,rowNumber)=>{
+//         row.eachCell((cell,colNumber)=>{
+//             if(cell.value=== 'Kivi'){
+//                 console.log(rowNumber);
+//                 console.log(colNumber);
+//             }
+//         })
+//     })
+// }
+
+// writeUpdateExcel();
+
+/* Write files */
+//let rowVal,colVal;
+async function excelWriteFile() {
+
+    let output={
+        "row": -1,
+        "column": -1
+    }
+
+    const workbook = new excelJs.Workbook();
     await workbook.xlsx.readFile("C:/Users/rohit/Downloads/excel_download_test.xlsx");
-    const worksheet = await workbook.getWorksheet('Sheet1');
-
+    const worksheet= workbook.getWorksheet('Sheet1');
     worksheet.eachRow((row,rowNumber)=>{
         row.eachCell((cell,colNumber)=>{
-            console.log(cell.value);
+            if(cell.value==='Orange'){
+                output.row=rowNumber;
+                output.column=colNumber;
+
+                console.log(" row Value: "+ output.row +" Column Value is: "+ output.column);
+            }
+
         });
-    });
+    })
+
+    const cell =worksheet.getCell(output.row,output.column);
+    cell.value = 'Knight Services';
+
+    await workbook.xlsx.writeFile("C:/Users/rohit/Downloads/excel_download_test.xlsx");
+
 }
 
-
-excelTest();
+excelWriteFile();
