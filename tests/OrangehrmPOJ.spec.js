43cdecfd4cf7d5cb01601e2ea1e373674473cc99
Rohith kumar R <rohithkumarrakasy@gmail.com>
2026-07-07 19:12:10 -0700

    feat: Create Custom Fixtures

--- tests/OrangehrmPOJ.spec.js
+++ tests/OrangehrmPOJ.spec.js
@@ -1,69 +1,81 @@
-const { test, chromium, expect, firefox } = require("@playwright/test");
-const { POManager } = require("../pageObjects/POManager");
+// const { test, chromium, expect, firefox } = require("@playwright/test");
+// const { POManager } = require("../pageObjects/POManager");
+
+ const {test,expect} =require('../utils/TestFixture');
+
+// const {test,expect} =require('../utils/LoginTestFixture');
 
 // convert JSON --> String --> Js object
-const testData = JSON.parse(JSON.stringify(require("../utils/OrangehrmPOJTestData.json")));
+const testData = JSON.parse(
+  JSON.stringify(require("../utils/OrangehrmPOJTestData.json")),
+);
 
+for (const data of testData) {
+  test(`Validate Orange HRM portal for POJ model Implementation ${data.username}`, async () => {
+    // const browser = await chromium.launch({ headless: false });
+    const browser = await firefox.launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    const poManager = new POManager(page);
+    const loginpage = await poManager.getLoginpage();
+    const dashboardPage = await poManager.getDashboardPage();
+    const adminPage = await poManager.getAdminPage();
 
-test("Validate Orange HRM portal for POJ model Implementation", async () => {
-  // const browser = await chromium.launch({ headless: false });
-  const browser = await firefox.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  const poManager = new POManager(page);
-  const loginpage =await  poManager.getLoginpage();
-  const dashboardPage =await poManager.getDashboardPage();
-  const adminPage= await poManager.getAdminPage();
-  
-  const url =
-    "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+    const url =
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
 
-  await loginpage.navigateToUrl(url);
+    await loginpage.navigateToUrl(url);
 
-  await page.waitForLoadState("domcontentloaded");
+    await page.waitForLoadState("domcontentloaded");
 
-  await loginpage.validateHeader("Log");
+    await loginpage.validateHeader("Log");
 
-  
+    const fetchCredentials = await loginpage.getCredentials();
 
-  const fetchCredentials = await loginpage.getCredentials();
+    console.log("UserName Credentials: " + fetchCredentials.userName);
+    console.log("UserName Credentials: " + fetchCredentials.password);
 
-  console.log("UserName Credentials: " + fetchCredentials.userName);
-  console.log("UserName Credentials: " + fetchCredentials.password);
+    // await loginpage.validateLoginPage(
+    //   fetchCredentials.userName,
+    //   fetchCredentials.password,
+    // );
 
-  // await loginpage.validateLoginPage(
-  //   fetchCredentials.userName,
-  //   fetchCredentials.password,
-  // );
+    await loginpage.validateLoginPage(data.username, data.password);
 
-  await loginpage.validateLoginPage(
-    testData.usernamef,
-    testData.password
-  );
+    await dashboardPage.validateDashboardHeader("Dash");
 
-  await dashboardPage.validateDashboardHeader("Dash");
+    const menuList = await dashboardPage.getDashboardMenu();
+    console.log(menuList);
 
-  const menuList= await dashboardPage.getDashboardMenu();
-  console.log(menuList);
+    //Click on Admin to Navigate inside
 
-  //Click on Admin to Navigate inside
+    await dashboardPage.adminModule();
 
-  await dashboardPage.adminModule();
+    //fetch all menu contents from Admin module
+    const values = await adminPage.fetchAdminMenuContents();
+    console.log("Admin: " + values);
 
-  //fetch all menu contents from Admin module
-  const values= await adminPage.fetchAdminMenuContents();
-  console.log("Admin: "+ values);
+    await adminPage.hoverOnMenuItems(2);
 
-  await adminPage.hoverOnMenuItems(2);
+    const orgMenuVal = await adminPage.fetchOrganizationMenu();
+    console.log("Organization: " + orgMenuVal);
 
-  const orgMenuVal=await adminPage.fetchOrganizationMenu();
-  console.log("Organization: "+ orgMenuVal);
+    await adminPage.hoverOnOrgItems(0);
 
-  await adminPage.hoverOnOrgItems(0);
+    await adminPage.validateGeneralInfoDetails("General Information");
 
-  await adminPage.validateGeneralInfoDetails("General Information");
+    await page.pause();
+  });
+}
 
-  await page.pause();
+// test.only('Test it through Fixture', async({page, testDataOrange})=>{
 
+//   console.log(testDataOrange.username);
+//   console.log(testDataOrange.password);
+// });
 
-});
+test.only('Validate Login functionality through Login Fixture',async({loginPage})=>{
+
+  await expect(loginPage.getByRole("heading", { name: "Dashboard" })).toBeVisible();
+})
+
