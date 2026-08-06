ae9e132f0db5007320239d6a177cf0c47c74bdee
Rohith kumar R <rohithkumarrakasy@gmail.com>
2026-08-05 23:11:36 -0700

    Perform select dropdown

--- tests/hyrtest.spec.js
+++ tests/hyrtest.spec.js
@@ -6,11 +6,27 @@ test('Perform All Testing scenarios in Hyr Tutorial Portal', async({page})=>{
 
     await page.waitForLoadState('domcontentloaded');
 
+    //Perform Dropdown Selection Select only one Value at a time
+
     const menuList =  await page.locator('#LinkList210 ul li a');
     menuList.filter({hasText:'Selenium Practice'}).hover();
     menuList.filter({hasText: 'Dropdowns'}).click();
 
-    await expect(page.getByRole('heading',{name: 'HTML Dropdowns'})).toHaveText('HTML Dropdowns');
+    const htmlHeading = await page.getByRole('heading',{name: 'HTML Dropdowns'});
+    await htmlHeading.waitFor({state: 'visible'});
+
+    await expect(htmlHeading).toHaveText('HTML Dropdowns');
+
+    await page.locator('select#course').selectOption('java');
+
+    await page.locator('select#course').selectOption({label: 'Python'});
+
+    //Select Multiple values at a time
+    await page.locator('select#ide').selectOption([
+        'ec','ij','vs','nb'
+    ]);
+
+    await page.locator('select#ide').selectOption({label: 'NetBeans'});
 
     await page.pause();
 
